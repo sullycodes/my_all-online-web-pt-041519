b0d0fdcbcda9ef522777365c8916423df7cb48c6
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-10 15:43:32 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,7 +7,7 @@ def my_all?(collection)
     return_values << yield(collection[i])
     i += 1 
   end
-  puts return_values
+  print return_values
 end
 
 my_all?([1,2,3,4,5,6,7,8,9,10]) { |i| i < 5 }
