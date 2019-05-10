e4f18cd6a026a2ab85102915f32f1193f0da52d8
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-10 17:02:20 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -9,10 +9,11 @@ def my_all?(collection)
   end
   return_values.each { |n| 
     if n == "false"
-    false
-  else
-    true 
-  end
+      false
+    else
+      true 
+    end
+  }
 end
 
 my_all?([1,2,3,4,5,6,7,8,9,10]) { |i| 
