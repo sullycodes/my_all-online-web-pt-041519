555a4c0681773aa0bc80fa5b7bb9bb4c54ecad38
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-10 15:41:04 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,9 +3,9 @@ require 'pry'
 def my_all?(collection)
   i = 0 
   while i < collection.length
-    yield(collection[i])
+    puts yield(collection[i])
     i += 1 
   end
 end
 
-puts my_all?([1,2,3,4,5,6,7,8,9,10]) { |i| i < 5 }
+my_all?([1,2,3,4,5,6,7,8,9,10]) { |i| i < 5 }
